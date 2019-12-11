c242c88446dbf0621665c6f59fa451c2ca84121f
kjells <kjells@uw.edu>
2019-12-11 15:24:09 -0800

    update versions in setup.py, add globals file, first step for artifact, wire into cli

---
+++ src/vivarium_conic_lsff/globals.py
@@ -0,0 +1,26 @@
+
+CLUSTER_PROJECT = 'proj_cost_effect_conic'
+PROJECT_NAME = 'vivarium_conic_lsff'
+
+LOCATIONS = ['India', 'Nigeria', 'Ethiopia']
+
+CAUSE_MEASLES = 'measles'
+CAUSE_DIARRHEAL = 'diarrheal_diseases'
+CAUSE_LOWER_RESPIRATORY_INFECTIONS = 'lower_respiratory_infections'
+CAUSE_NEURAL_TUBE_DEFECTS = 'neural_tube_defects'
+CAUSE_NEONATAL_ENCEPHALOPATHY = 'neonatal_encephalopathy_due_to_birth_asphyxia_and_trauma'
+CAUSE_NEONATAL_SEPIS = 'neonatal_sepsis_and_other_neonatal_infections'
+
+CAUSES_WITH_INCIDENCE = [
+    CAUSE_MEASLES,
+    CAUSE_DIARRHEAL,
+    CAUSE_LOWER_RESPIRATORY_INFECTIONS,
+]
+
+CAUSES_NEONATAL = [
+    CAUSE_NEURAL_TUBE_DEFECTS,
+    CAUSE_NEONATAL_ENCEPHALOPATHY,
+    CAUSE_NEONATAL_SEPIS,
+]
+
+
