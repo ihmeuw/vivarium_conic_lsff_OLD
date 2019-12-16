36fd5af5805bb1227ed035127b6a0e75dece932b
kjells <kjells@uw.edu>
2019-12-16 14:10:20 -0800

    don't duplicate gbd mapping strings

--- src/vivarium_conic_lsff/globals.py
+++ src/vivarium_conic_lsff/globals.py
@@ -1,16 +1,18 @@
+from gbd_mapping import causes
+
 
 CLUSTER_PROJECT = 'proj_cost_effect_conic'
 PROJECT_NAME = 'vivarium_conic_lsff'
 
 LOCATIONS = ['India', 'Nigeria', 'Ethiopia']
 
-CAUSE_MEASLES = 'measles'
-CAUSE_DIARRHEAL = 'diarrheal_diseases'
-CAUSE_LOWER_RESPIRATORY_INFECTIONS = 'lower_respiratory_infections'
-CAUSE_NEONATAL_NEURAL_TUBE_DEFECTS = 'neural_tube_defects'
-CAUSE_NEONATAL_ENCEPHALOPATHY = 'neonatal_encephalopathy_due_to_birth_asphyxia_and_trauma'
-CAUSE_NEONATAL_SEPSIS = 'neonatal_sepsis_and_other_neonatal_infections'
-CAUSE_NEONATAL_JAUNDICE = 'hemolytic_disease_and_other_neonatal_jaundice'
+CAUSE_MEASLES = causes.cause.measles.name
+CAUSE_DIARRHEAL = causes.cause.diarrheal_diseases.name
+CAUSE_LOWER_RESPIRATORY_INFECTIONS = causes.cause.lower_respiratory_infections.name
+CAUSE_NEONATAL_NEURAL_TUBE_DEFECTS = causes.cause.neural_tube_defects.name
+CAUSE_NEONATAL_ENCEPHALOPATHY = causes.cause.neonatal_encephalopathy_due_to_birth_asphyxia_and_trauma.name
+CAUSE_NEONATAL_SEPSIS = causes.cause.neonatal_sepsis_and_other_neonatal_infections.name
+CAUSE_NEONATAL_JAUNDICE = causes.cause.hemolytic_disease_and_other_neonatal_jaundice.name
 
 CAUSES_WITH_INCIDENCE = [
     CAUSE_MEASLES,
